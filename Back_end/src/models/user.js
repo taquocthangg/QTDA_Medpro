663c32fd3e2c99ce00726fd3853c920d4a4ac0d4
taquocthangg <quocthangforwork@gmail.com>
2024-04-15 01:22:06 +0700

    update_models_user

--- Back_end/src/models/user.js
+++ Back_end/src/models/user.js
@@ -10,7 +10,6 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
       User.belongsTo(models.Sescription, {
         foreignKey: 'id_chuyenKhoa',
       });
